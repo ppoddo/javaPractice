23a4c57c5e50f3f6ebaed91ec23b9447145f81fb
ppoddo <lhj950720@naver.com>
2023-09-04 20:16:10 +0900

    Hi intellij

--- 00_myFirstProject/src/com/kh/first/HelloWorld.java
+++ 00_myFirstProject/src/com/kh/first/HelloWorld.java
@@ -2,7 +2,7 @@ package com.kh.first;
 
 public class HelloWorld {  //클래스
 	public static void main(String[] args) { //메소드
-		System.out.println("Hi, Everyone!");   //출력문
+		System.out.println("Hi, everyon www");   //출력문
 		
 				
 		
